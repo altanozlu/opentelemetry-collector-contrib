28efbd9979713d7776f17df1397d8df58154daca
Altan Özlü <5479094+altanozlu@users.noreply.github.com>
2022-02-16 18:04:22 +0300

    try as info

--- exporter/sentryexporter/sentry_exporter.go
+++ exporter/sentryexporter/sentry_exporter.go
@@ -184,7 +184,7 @@ func sentryEventFromError(errorMessage, errorType string, span *sentry.Span) (*s
 
 	event.Type = errorType
 	event.Message = errorMessage
-	event.Level = "error"
+	event.Level = "info"
 	event.Exception = []sentry.Exception{{
 		Value: errorMessage,
 		Type:  errorType,
